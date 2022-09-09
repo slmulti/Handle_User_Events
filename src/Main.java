d2e179e1620d83bb3d6f8295d25cfa8e63650732
Simon Love <simon-love@hotmail.co.uk>
2022-09-09 12:35:41 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application implements EventHandler<ActionEvent> {
+
+    Button button;
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("My First Window");
+        button = new Button();
+        button.setText("Click Me");
+
+        button.setOnAction(this);
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300,250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        if(actionEvent.getSource()==button){
+            System.out.println("Clicky McCLickFace");
+        }
+    }
+}
